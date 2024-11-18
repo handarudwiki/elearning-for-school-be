8f90fa2c3ebe6f4a454fada8f9d5bde982abfca1
handarudwiki <handarudwiki04@gmail.com>
2024-11-18 15:52:24 +0700

    feat create table classroom_subjects

---
+++ database/migrations/20241118084649_creare_classroom_subjects_table.sql
@@ -0,0 +1,16 @@
+-- +goose Up
+-- +goose StatementBegin
+CREATE TABLE classroom_subject(
+    id SERIAL PRIMARY KEY,
+    classroom_id INT NOT NULL REFERENCES classrooms(id) ON DELETE CASCADE,
+    subject_id INT NOT NULL REFERENCES subjects(id) ON DELETE CASCADE,
+    teacher_id INT NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+DROP TABLE if exists classroom_subject CASCADE;
+-- +goose StatementEnd
